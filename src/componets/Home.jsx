c1fab724646dc9b7f82c5a4efed4815aca97f2ad
chittaranjan.... <148612770+Chittaranjans@users.noreply.github.com>
2024-09-26 14:28:21 +0530

    Update Home.jsx

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { SpeechRecognition, useSpeechRecognition } from 'react-speech-recognition';
 
 const Home = () => {
   const {
