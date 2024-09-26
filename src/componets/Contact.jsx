cec7d8546c8cbed2e0df0f2da1836ba721d8c68d
chittaranjan.... <148612770+Chittaranjans@users.noreply.github.com>
2024-09-26 14:20:23 +0530

    Update Contact.jsx

--- src/componets/Contact.jsx
+++ src/componets/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
     setPassword(password)
   }, [charecter, hasNumber, setPassword ,length])
 
-  useEffect(() => {handelPassword()}, [setPassword , length , hasNumber , charecter])
+  // useEffect(() => {handelPassword()}, [setPassword , length , hasNumber , charecter])
 
   const copy = useCallback(() => { 
     firstRender.current.select()
